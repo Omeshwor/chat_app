e07773715ac9afc4253d0ceaa0f82720c99e4580
Omeshwor Shrestha <omeshworlalshrestha@yahoo.com>
2019-12-20 15:20:31 -0600

    Add user new functionality

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,17 +1,33 @@
 class UsersController < ApplicationController
-  def index
-      @users = User.paginate(page: params[:page], per_page: 5`)
-  end
+    def index
+        @users = User.paginate(page: params[:page], per_page: 5)
+    end
 
-  def new
-  end
+    def new
+        @user = User.new
+    end
 
-  def create
-  end
+    def create
+        @user = User.new(user_params)
+        if @user.save
+          session[:user_id] = @user.id
+          flash[:success] = "Welcome to the chatroom #{@user.username}"
+          redirect_to user_path(@user)
+        else
+          render 'new'
+        end
+    end
 
-  def show
-  end
+    def show
+    end
 
-  def edit
-  end
+    def edit
+    end
+
+    private
+
+    def user_params
+        params.require(:user).permit(:username, :password, :password_confirmation)
+    end
+
 end
