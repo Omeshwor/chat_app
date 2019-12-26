5c8e9e72c53ad96a3801133d31cc7d7f932939e0
Omeshwor Shrestha <omeshworlalshrestha@yahoo.com>
2019-12-26 00:27:07 -0600

    Add user_id to session after signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,7 +10,7 @@ class UsersController < ApplicationController
     def create
         @user = User.new(user_params)
         if @user.save
-          # session[:user_id] = @user.id
+          session[:user_id] = @user.id
           flash[:positive] = "Welcome to the chatroom #{@user.username}"
           redirect_to user_path(@user)
         else
