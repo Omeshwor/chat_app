216670e0aa8adee9dee8b11611a7791c4465087b
Omeshwor Shrestha <omeshworlalshrestha@yahoo.com>
2019-12-26 00:26:04 -0600

    Add user login functionality

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,4 +2,21 @@ class SessionsController < ApplicationController
     def new
     end
 
+    def create
+        user = User.find_by(username: params[:session][:username].downcase)
+        if user && user.authenticate(params[:session][:password])
+            session[:user_id] = user.id
+            flash[:positive] = "You have successfully signed in!"
+            redirect_to user_path(user)
+        else
+            flash.now[:negative] = "Invalid username/password"
+            render 'new'
+        end
+    end
+
+    def destroy
+    end
+
+
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
     has_secure_password
+    before_save { self.downcase = username.downcase }
     validates :username, presence: true, uniqueness: {case_sensitive: false}, length: {minimum: 3, maximum: 12}
     validates :password, presence: true, length: {minimum: 6, maximum: 8}
 
