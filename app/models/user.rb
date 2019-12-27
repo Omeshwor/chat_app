a188f2ac31d8295d52f7f800e6979070bafa89b2
Omeshwor Shrestha <omeshworlalshrestha@yahoo.com>
2019-12-27 00:03:06 -0600

    Add message model and link it with user

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,6 @@ class User < ApplicationRecord
     before_save { self.username = username.downcase }
     validates :username, presence: true, uniqueness: {case_sensitive: false}, length: {minimum: 3, maximum: 12}
     validates :password, presence: true, length: {minimum: 6, maximum: 8}
+    has_many :messages
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_19_201515) do
+ActiveRecord::Schema.define(version: 2019_12_27_055449) do
+
+  create_table "messages", force: :cascade do |t|
+    t.integer "user_id"
+    t.text "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_messages_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "username"
